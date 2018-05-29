312c96a8aa0f6b63e574a28a5bd8827c41eed406
zhouqingyi616 <zhouqingyi616@126.com>
2018-05-29 22:28:01 +0800

    concat_result
    stride=100, seg=300.

--- concat_result.py
+++ concat_result.py
@@ -11,7 +11,10 @@ EPOCH_NUM = 1
 MODEL_PATH = "models/V1.1/U_Net.pkl"
 N_CHANNEL = 3
 N_CLASS = 2
-BATCH_NUM = 24 * 24
+stride = 100
+seg = 300
+L_NUM = 48
+BATCH_NUM = 48 * 48
 TEST_CITY_NAME = ["bellingham", "bloomington", "innsbruck", "sfo", "tyrol-e"]
 TEST_RESULT_PATH = "final_data/test/output/"
 
@@ -20,47 +23,48 @@ print(torch.cuda.is_available())
 
 my_model = torch.load(MODEL_PATH).cuda()
 print("model from load")
-# print(my_model)
+print(my_model)
 
 criterion = torch.nn.NLLLoss()
 
 avg_num = np.ones([5000, 5000], np.int)
-avg_num[200:4800, 200:4800] = 4
-avg_num[0, 200:4800] = 2
-avg_num[200:4800, 0] = 2
-avg_num[4999, 200:4800] = 2
-avg_num[200:4800, 4999] = 2
+for i in range(L_NUM):
+    for j in range(L_NUM):
+        avg_num[i*stride:i*stride+seg, j*stride:j*stride+seg] += 1
 
 avg_prob = np.zeros([5000, 5000])  # Final submit prob
 
 # get result
 for i, (images, labels) in enumerate(test_loader):
-    city_id = i // (36*24*24)
-    big_id = (i % (36*24*24)) // (24*24)
-    tmp_row = (i % (24*24)) // 24
-    tmp_col = (i % (24*24)) % 24
-    print("city:", city_id, "big:", big_id, "row:", tmp_row, "col:", tmp_col)
+    city_id = i // (36*BATCH_NUM)
+    big_id = (i % (36*BATCH_NUM)) // BATCH_NUM
+    tmp_row = (i % BATCH_NUM) // L_NUM
+    tmp_col = (i % BATCH_NUM) % L_NUM
     
-    images = Variable(images).cuda()
+    images = Variable(images)
     
     # img_show = np.squeeze(images.numpy(), 0)
     # img_show = img_show.transpose((1, 2, 0))
     # cv2.imshow("tmp_win1", img_show)
-    # cv2.waitKey(1)
+    # cv2.waitKey(0)
     
+    if torch.cuda.is_available():
+        images = images.cuda()
+
     # Forward
     outputs = my_model(images)
-    img_out = np.squeeze(outputs.cpu().detach().numpy(), 0)
+    img_out = np.squeeze((outputs.detach()).cpu().numpy(), 0)
     img_out = img_out.transpose((1, 2, 0))
     prob = np.exp(img_out[:, :, 1])
-    avg_prob[tmp_row*200:(tmp_row*200+400), tmp_col*200:(tmp_col*200+400)] += prob
+
+avg_prob[tmp_row*stride:tmp_row*stride+seg, tmp_col*stride:tmp_col*stride+seg] += prob
     
     # Show result for small img
     # prob[prob < 0.5] = 0
     # prob[prob >= 0.5] = 1
     # img_print = 255.0 * prob
     # cv2.imshow("tmp_win2", img_print)
-    # cv2.waitKey(1)
+    # cv2.waitKey(0)
     
     if (i+1) % BATCH_NUM == 0:
         avg_prob = avg_prob / avg_num
@@ -68,5 +72,5 @@ for i, (images, labels) in enumerate(test_loader):
         avg_prob[avg_prob >= 0.5] = 1
         img_submit = 255 * avg_prob
         cv2.imwrite(TEST_RESULT_PATH + TEST_CITY_NAME[city_id] + str(big_id + 1) + ".tif", img_submit)
-        print("Saving...")
+        print("Saving")
         avg_prob = np.zeros_like(avg_prob)
